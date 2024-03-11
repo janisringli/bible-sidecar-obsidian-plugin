bf42bb2bd744883d0befac34b2d75e7f2d84b462
Janis Ringli <janis.ringli@gmail.com>
2024-03-11 14:12:44 +0100

    concatinated all the verses inside a chapter for better style opportunities

--- BibleView.ts
+++ BibleView.ts
@@ -1,11 +1,4 @@
-
-import {
-	ItemView,
-	WorkspaceLeaf,
-	requestUrl,
-	App,
-	
-} from "obsidian";
+import { ItemView, WorkspaceLeaf, requestUrl, App } from "obsidian";
 
 export const BibleViewType = "bible-view";
 
@@ -31,27 +24,52 @@ export class BibleView extends ItemView {
 		const url = "https://bolls.life/get-books/NLT";
 		const response = await requestUrl(url);
 		const { containerEl } = this;
-		
+		let scripture: string;
+
 		containerEl.empty();
 		containerEl.createEl("h2", { text: "Bible View" });
-		const ChapterWrapper = containerEl.createEl("div", { cls: "bible-wrapper" });
-		const chapterContainer = ChapterWrapper.createEl("div" , { cls: "chapter-container" });
+		const ChapterWrapper = containerEl.createEl("div", {
+			cls: "bible-wrapper",
+		});
+		const chapterContainer = ChapterWrapper.createEl("div", {
+			cls: "chapter-container",
+		});
 		for (const book of response.json) {
 			console.log(book.name);
-			chapterContainer.createEl("button", { text: book.name , attr: { id: book.bookid }}).addEventListener("click", async () => {
-				chapterContainer.empty();
-				for (let i = 1; i <= book.chapters; i++) {
-					chapterContainer.createEl("button", { text: i.toString(), cls: "chapter-button" }).addEventListener("click", async () => {
-						const chapterContent = await this.getChapterContent(book.bookid, i);
-						chapterContainer.empty();
-						chapterContainer.createEl("h2", { text: `${book.name} ${i}` });
-						for (const verse of chapterContent) {
-							chapterContainer.createEl("span", { text: verse.text });
-						}
-						// Now you can do whatever you want with the chapter content
-					});
-				}
-			});
+			chapterContainer
+				.createEl("button", {
+					text: book.name,
+					attr: { id: book.bookid },
+				})
+				.addEventListener("click", async () => {
+					chapterContainer.empty();
+					for (let i = 1; i <= book.chapters; i++) {
+						chapterContainer
+							.createEl("button", {
+								text: i.toString(),
+								cls: "chapter-button",
+							})
+							.addEventListener("click", async () => {
+								const chapterContent =
+									await this.getChapterContent(
+										book.bookid,
+										i
+									);
+								chapterContainer.empty();
+								chapterContainer.createEl("h2", {
+									text: `${book.name} ${i}`,
+								});
+								for (const verse of chapterContent) {
+									scripture += verse.verse + " ";
+									console.log(scripture);
+								}
+								chapterContainer.createEl("p", {
+									text: scripture,
+								});
+								// Now you can do whatever you want with the chapter content
+							});
+					}
+				});
 		}
 	}
 
