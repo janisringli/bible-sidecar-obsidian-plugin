4a23d04bc25c83b5cb3ba6f76d6e41af3427a2fd
Janis Ringli <janis.ringli@gmail.com>
2024-03-13 14:39:29 +0100

    flawless functionality of chaptercontrols

--- BibleView.ts
+++ BibleView.ts
@@ -14,6 +14,8 @@ const DEFAULT_SETTINGS: Partial<BibleSidecarSettings> = {
 export class BibleView extends ItemView {
     settings: BibleSidecarSettings;
     backButton: HTMLElement;
+	previousButton: HTMLElement;
+	nextButton: HTMLElement;
 
     constructor(leaf: WorkspaceLeaf) {
         super(leaf);
@@ -48,7 +50,7 @@ export class BibleView extends ItemView {
         return response.json;
     }
 
-    async getChapterContent(bookid: number, chapter: number) {
+    async getChapterContent(bookid: number, chapter: number,) {
         const url = `https://bolls.life/get-chapter/${this.settings.bibleVersion}/${bookid}/${chapter}`;
         console.log(url);
         const response = await requestUrl(url);
@@ -73,26 +75,31 @@ export class BibleView extends ItemView {
 
         const ChapterWrapper = containerEl.createEl("div", { cls: "bible-wrapper" });
         const chapterContainer = ChapterWrapper.createEl("div", { cls: "chapter-container" });
+		const chapterContent = chapterContainer.createEl("div", { cls: "chapter-content" });
 
         for (const book of books) {
-            console.log(book.name);
+			if (book.bookid === 1) {
+				chapterContainer.createEl("h4", { text: "Old Testament", cls: "book-divider" });
+			}
 			if (book.bookid === 40) {
 				chapterContainer.createEl("hr", { cls: "book-divider" });
+				chapterContainer.createEl("h4", { text: "New Testament", cls: "book-divider" });
 			}
             chapterContainer
                 .createEl("button", { text: book.name, attr: { id: book.bookid } })
                 .addEventListener("click", async () => {
-                    await this.renderChapters(book, chapterContainer);
+                    await this.renderChapters(book, chapterContainer, books);
                 });
         }
     }
 
-    async renderChapters(book: any, chapterContainer: HTMLElement) {
-        let scriptureRaw: string = "";
+    async renderChapters(book: any, chapterContainer: HTMLElement, books: any[]) {
         chapterContainer.empty();
-
+		
         // Header with back button
         const header = chapterContainer.createDiv({ cls: "bible-header" });
+		const controlsContainer = chapterContainer.createDiv({ cls: "controls-container" });
+		
         this.backButton = header.createEl("button", { text: "Back to Books", cls: "back-button" });
         this.backButton.addEventListener("click", () => {
             this.onOpen();
@@ -102,18 +109,72 @@ export class BibleView extends ItemView {
             chapterContainer
                 .createEl("button", { text: i.toString(), cls: "chapter-button" })
                 .addEventListener("click", async () => {
-                    const chapterContent = await this.getChapterContent(book.bookid, i);
+                    const chapterContentArray = await this.getChapterContent(book.bookid, i);
                     chapterContainer.empty();
-                    chapterContainer.createEl("h2", { text: `${book.name} ${i}` });
-                    for (const verse of chapterContent) {
-                        let formattedVerseNumber = this.convertToSuperscript(verse.verse);
-                        scriptureRaw += `${formattedVerseNumber} ${verse.text} `;
-                    }
-                    console.log(scriptureRaw);
-                    let editedScripture = scriptureRaw.replace(/<br>/g, "\n");
-                    console.log(editedScripture);
-                    chapterContainer.createEl("p", { text: editedScripture });
+                    
+                    this.processChapterContent(chapterContentArray, chapterContainer, book, i,books);
                 });
         }
     }
+	processChapterContent(chapter: { verse: string; text: string }[], chapterContainer: HTMLElement, book: any, i: number, books: any[]) {
+		console.log("chapter number at start of processChapterContent", i);
+		console.log("book")
+		console.log(book)
+		chapterContainer.createEl("h2", { text: `${book.name} ${i}` });
+		const controlsContainer = chapterContainer.createDiv({ cls: "controls-container" });
+					this.previousButton = controlsContainer.createEl("button", { text: "Previous chapter", cls: "back-button" });
+					this.previousButton.addEventListener("click", async () => {
+						let newChapter = i - 1;
+						if (newChapter < 1) {
+							newChapter = 1;
+						} else {
+						
+						let previousChapterContent = await this.getChapterContent(book.bookid, newChapter);
+						console.log(previousChapterContent)
+						chapterContainer.empty();
+						this.processChapterContent(previousChapterContent, chapterContainer,book, newChapter, books);
+						}
+
+					});
+					this.backButton = controlsContainer.createEl("button", { text: "Back to Books", cls: "back-button" });
+					this.backButton.addEventListener("click", () => {
+						this.onOpen();
+					});
+					this.nextButton = controlsContainer.createEl("button", { text: "Next chapter", cls: "back-button" });
+					this.nextButton.addEventListener("click", async () => {
+						//todo: add logic to go to next chapter
+						let newChapter = i + 1;
+						if (newChapter > book.chapters) {
+
+							let newBookId = book.bookid + 1;
+							let newBook = books[newBookId - 1];
+							newChapter = 1;
+							let nextChapterContent = await this.getChapterContent(newBookId, newChapter);
+							chapterContainer.empty();
+							this.processChapterContent(nextChapterContent, chapterContainer,newBook, newChapter, books);
+						}else{
+						const nextChapterContent = await this.getChapterContent(book.bookid, newChapter);
+						console.log("next chapter content", nextChapterContent);
+						
+						chapterContainer.empty();
+						this.processChapterContent(nextChapterContent, chapterContainer,book, newChapter, books);
+						}
+					});
+		const chapterContent = chapterContainer.createEl("div", { cls: "chapter-content" });
+		chapterContent.empty();
+		
+					
+		let scriptureRaw: string = '';
+		
+		for (const verse of chapter) {
+			let formattedVerseNumber = this.convertToSuperscript(verse.verse);
+			scriptureRaw += `${formattedVerseNumber} ${verse.text} `;
+		}
+		let editedScripture = scriptureRaw.replace(/<br>/g, "\n");
+		const scripture = chapterContent.createEl("p", { text: editedScripture });
+	
+		
+	}
+	
+	
 }
